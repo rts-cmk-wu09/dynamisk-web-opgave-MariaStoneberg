e9bfe650d948b027f973f8cb6462126ea2edab17
MariaStoneberg <U2430198@rts-365.dk>
2023-02-13 14:15:26 +0100

    Add facilities to project

--- js/index.js
+++ js/index.js
@@ -2,15 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // slå dig løs her... 
     document.querySelector(".hero").innerHTML = `
-    <h1>
-        ${hero.headline}
-    </h1>
-    <p>
-        ${hero.copy}
-    </p>
-    <button>
-        ${hero.icon}
+    <figure class="hero-img">
+    <img src="${hero.image}" alt="">
+    </figure>
+
+    <section class="hero-container">
+    <h1>${hero.headline}</h1>
+    <p>${hero.copy}</p>
+    <button class="hero-btn">
+    <img src="${hero.icon}" alt="">
+    <p>Explore</p>
     </button>
+    </section>
     
     `;
 
@@ -19,13 +22,15 @@ document.addEventListener("DOMContentLoaded", function() {
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
 
+
+    /* Services */
     let servicesSection = document.querySelector(".services");
     
     services.forEach((service) => {
         let card = document.createElement("section");
         card.classList = "servicesCard"
         let image = document.createElement("img");
-        let title = document.createElement("h3");
+        let title = document.createElement("h2");
         let cardText = document.createElement("p");
         let linktext = document.createElement("a");
 
@@ -41,6 +46,34 @@ document.addEventListener("DOMContentLoaded", function() {
         servicesSection.append(card);
 
     });
+
+    /* Facilities */
+
+    let facilitiesSection = document.querySelector(".facilities");
+    
+        facilities.options.forEach((option) => {
+        let card = document.createElement("section");
+        card.classList = "optionCard"
+        let image = document.createElement("img");
+        let title = document.createElement("h2");
+        let cardText = document.createElement("p");
+
+        image.src = option.icon;
+        title.textContent = option.headline;
+        cardText.textContent = option.text;
+
+        card.append(image); 
+        card.append(title);
+        card.append(cardText);
+        facilitiesSection.append(card);
+
+    });
+
+
+
+
+
+    
     
 }) // DOMContentLoaded slut
 
